ffaa872d9900f67497fe7d254e44da7b9269d754
Liwa <liwa.lemjid@esprit.tn>
2018-08-08 02:37:26 +0100

    Initial commit

---
+++ app/src/main/java/com/dev/liwa/dinnerdecider/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dev.liwa.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf<String>("Chinese","Hamburger","Pizza","KFC")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decidebtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodbtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
+    }
+}
